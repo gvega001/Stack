054966cbfe0cb2d82f61ae076f7750c537fed60d
Gabriel Vega <gvega001@gmail.com>
2020-07-21 20:18:22 -0700

    Add files via upload
    1st commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Stack1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack theStack = new Stack(4);
+            theStack.push("Star Wars");
+            theStack.push("Jaws");
+            theStack.push("Dumb and Dumber");
+            theStack.push("Jaws 2");
+            theStack.push("Ace Ventura");
+           
+
+            WriteLine("=================\n About to peek:\n");
+
+            WriteLine("'"+theStack.peek() + "'"+"is as the top of the stack");
+
+
+            WriteLine("=================\n The Stack contains:\n");
+
+            while (!theStack.isEmpty() )
+            {
+                string movie = theStack.pop();
+                WriteLine(movie);  
+            }
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static System.Console;
+namespace Stack1
+{
+    public class Stack
+    {
+        private int maxSize;
+        private string[] stackArray;
+        private int top;
+        public Stack()
+        {
+
+        }
+        public Stack(int size)
+        {
+            maxSize = size;
+            stackArray = new string[maxSize];
+            top = -1;
+        }
+        public void push(string m)
+        {
+            if (isFull())
+            {
+                WriteLine("This stack is full");
+            }
+            else
+            {
+                top++;
+                stackArray[top] = m;
+            }
+        }
+        private bool isFull()
+        {
+            return (maxSize - 1 == top);
+        }
+        public string pop()
+        {
+            if (isEmpty())
+            {
+                WriteLine("The stack is empty.");
+                return "--";
+            }
+            else
+            {
+                int oldTop = top;
+                top--;
+                return stackArray[oldTop];
+            }
+        }
+        public string peek()
+        {
+            return stackArray[top];
+        }
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+    }
+}
